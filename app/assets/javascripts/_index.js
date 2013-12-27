131709f049b233330ed40228aab2187db43620d6
Timothy Lee <timmy.ccl@gmail.com>
2013-12-27 20:35:48 +0800

    List item from api

--- app/assets/javascripts/_index.js
+++ app/assets/javascripts/_index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    // {{{ Events
+    // {{{ Global Events
     $(window).on('resize', function(ev){
         var left = $('.list-left');
         var right = $('.list-right');
@@ -9,39 +9,56 @@ $(document).ready(function(){
         right.height($(window).height() - nav.height());
         right.width($(window).width() - left.width() - (8+1));
     });
+    // }}}
+    
+    // {{{ Events
+    var registerEvents = function() {
+        $('a.list-item').on('click', function(ev) {
+            var url = $(ev.currentTarget).data('url');
+            $('.iframe-container').attr('src', url);
+            return false;
+        });
 
-    $('a.list-item').on('click', function(ev) {
-        var url = $(ev.currentTarget).data('url');
-        $('.iframe-container').attr('src', url);
-        return false;
-    });
+        $('a.list-item').on('mouseover', function(ev) {
+            $(ev.currentTarget).addClass('focus-on');
+        });
 
-    $('a.list-item').on('mouseover', function(ev) {
-        $(ev.currentTarget).addClass('focus-on');
-    });
+        $('a.list-item').on('mouseleave', function(ev) {
+            $(ev.currentTarget).removeClass('focus-on');
+        });
 
-    $('a.list-item').on('mouseleave', function(ev) {
-        $(ev.currentTarget).removeClass('focus-on');
-    });
+        $('.list-left').on('click', function(ev) {
+            var left = $('.list-left');
+            var right = $('.list-right');
+            var nav = $('.navbar-inner');
 
-    $('.list-left').on('click', function(ev) {
-        var left = $('.list-left');
-        var right = $('.list-right');
-        var nav = $('.navbar-inner');
-
-        if (left.width() > 10) {
-            right.width(right.width() + left.width());
-            left.width(0);
-            left.css('cursor', 'e-resize');
-        } else {
-            left.width(230);
-            right.width(right.width() - left.width());
-            left.css('cursor', 'w-resize');
-        }
-    });
+            if (left.width() > 10) {
+                right.width(right.width() + left.width());
+                left.width(0);
+                left.css('cursor', 'e-resize');
+            } else {
+                left.width(230);
+                right.width(right.width() - left.width());
+                left.css('cursor', 'w-resize');
+            }
+        });
+    }
     // }}}
 
     $(window).trigger('resize');
-    $('.list-left a').first().click();
 
+    $.post('/api/list', function(list) {
+        $.each(list, function(i, item){
+            var a = $('<a></a>');
+            a.text(item.name);
+            a.attr('data-url', item.url);
+            a.attr('href', '#');
+            a.addClass('list-item');
+            var li = $('<li></li>');
+            li.append(a);
+            $('.list-left ul').append(li);
+        });
+        registerEvents();
+        $('a.list-item').first().click();
+    });
 });
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ EatgoDashboard::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   #root :to => 'high_voltage/pages#show', :id => 'welcome'
   root :to => 'index#show'
+  
+  resources 'api', only: [] do
+    collection do
+      post :list
+      get :list
+    end
+  end
 end
