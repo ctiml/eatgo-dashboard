4cc12393d0ef4e454bd4e87ff2bb2843a7aa5e67
Timothy Lee <timmy.ccl@gmail.com>
2013-12-27 11:52:48 +0800

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EatgoDashboard::Application.config.secret_key_base = '3c47bb76ab3126b51bb2f60c3247ffa4c35014ae1acfd2fd1a4e73ad39fec3314101613a7fd98f66e43147626a400df5643dc8639bea82e6c9e28bfd6b37f20b'
