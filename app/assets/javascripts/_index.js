cddb69eec18fd57123b843eee781266eb0ec8d71
Timothy Lee <timmy.ccl@gmail.com>
2013-12-27 19:38:52 +0800

    refine

--- app/assets/javascripts/_index.js
+++ app/assets/javascripts/_index.js
@@ -1,6 +1,47 @@
 $(document).ready(function(){
-    $('.list-left a').on('click', function(ev) {
+
+    // {{{ Events
+    $(window).on('resize', function(ev){
+        var left = $('.list-left');
+        var right = $('.list-right');
+        var nav = $('.navbar-inner');
+        left.height($(window).height() - nav.height());
+        right.height($(window).height() - nav.height());
+        right.width($(window).width() - left.width() - (8+1));
+    });
+
+    $('a.list-item').on('click', function(ev) {
         var url = $(ev.currentTarget).data('url');
         $('.iframe-container').attr('src', url);
+        return false;
     });
+
+    $('a.list-item').on('mouseover', function(ev) {
+        $(ev.currentTarget).addClass('focus-on');
+    });
+
+    $('a.list-item').on('mouseleave', function(ev) {
+        $(ev.currentTarget).removeClass('focus-on');
+    });
+
+    $('.list-left').on('click', function(ev) {
+        var left = $('.list-left');
+        var right = $('.list-right');
+        var nav = $('.navbar-inner');
+
+        if (left.width() > 10) {
+            right.width(right.width() + left.width());
+            left.width(0);
+            left.css('cursor', 'e-resize');
+        } else {
+            left.width(230);
+            right.width(right.width() - left.width());
+            left.css('cursor', 'w-resize');
+        }
+    });
+    // }}}
+
+    $(window).trigger('resize');
+    $('.list-left a').first().click();
+
 });
